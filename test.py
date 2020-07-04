f81fa587821ca7e56190c272df3102a92e9248c0
Tucker <tuckermiles70@gmail.com>
2020-07-04 19:22:11 -0400

    Successfully showing the macs I was looking for

--- test.py
+++ test.py
@@ -11,14 +11,19 @@ nm = nmap.PortScanner()
 #You can then do a scan of all the IPV4 addresses provided by the network you are connected to
 nm.scan(hosts = '192.168.1.0/24', arguments = '-sn')
 
+# 88:19:08:2C:EE:BD
+
 # possibly send a text, an email, some sort of alert when a specified mac address is connected
 
 for host in nm.all_hosts():
-    #If the status of an IP address is not down, print it
-    if nm[host]['status']['state'] != "down":
-        print ("STATUS:", nm[host]['status']['state'])
-        #Print the MAC address
-        try:
-            print ("MAC ADDRESS:", nm[host]['addresses']['mac'])
-        except:
-            mac = 'unknown'
+    print(nm[host])
+
+# for host in nm.all_hosts():
+#     #If the status of an IP address is not down, print it
+#     if nm[host]['status']['state'] != "down":
+#         print ("STATUS:", nm[host]['status']['state'])
+#         #Print the MAC address
+#         try:
+#             print ("MAC ADDRESS:", nm[host]['addresses']['mac'])
+#         except:
+#             mac = 'unknown'
