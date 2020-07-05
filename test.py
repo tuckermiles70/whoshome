c6c796c57e3ecf9e9f0d2acfe94413161b48d854
Tucker <tuckermiles70@gmail.com>
2020-07-05 07:34:03 -0400

    Added macs, refactored search code

--- test.py
+++ test.py
@@ -1,29 +1,30 @@
 import nmap
 
-# nmScan = nmap.PortScanner()
-
-# print(nmScan.scan('127.0.0.1', '21-443'))
-
 # http://iltabiai.github.io/home%20automation/2015/09/11/npm-roommates.html
+
+known_macs = {
+    '18:F1:D8:96:AC:AD' : 'Tucker',
+    '88:19:08:2C:EE:BD' : 'Hayden'
+}
 
 #First, you need a Postscanner object that will be used to do the scan
 nm = nmap.PortScanner()
 #You can then do a scan of all the IPV4 addresses provided by the network you are connected to
 nm.scan(hosts = '192.168.1.0/24', arguments = '-sn')
 
-# 88:19:08:2C:EE:BD
-
 # possibly send a text, an email, some sort of alert when a specified mac address is connected
 
-for host in nm.all_hosts():
-    print(nm[host])
+for index, host in enumerate(known_macs):
+    # print(index)
+    print(host)
 
-# for host in nm.all_hosts():
-#     #If the status of an IP address is not down, print it
-#     if nm[host]['status']['state'] != "down":
-#         print ("STATUS:", nm[host]['status']['state'])
-#         #Print the MAC address
-#         try:
-#             print ("MAC ADDRESS:", nm[host]['addresses']['mac'])
-#         except:
-#             mac = 'unknown'
+print()
+
+for host in nm.all_hosts():
+    # print(nm[host])
+    if (nm[host]['status']['state'] == "up"):
+        try:
+            mac = nm[host]['addresses']['mac']
+        except:
+            mac = 'unknown'
+        print("mac {} is connected".format(mac))
