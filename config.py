090550921b98f48de3be94835a3485c20972f55f
Tucker <tmiles7@vols.utk.edu>
2020-07-19 16:47:24 -0400

    Cleanup

---
+++ config.py
@@ -0,0 +1,10 @@
+email_address = "SendingGmailAccount@Gmail.com"
+email_password = "YourAppPassword"
+
+email_list = ["Email1@email.com", "Email2@email.com", "Email3@email.com"]
+
+known_macs = {
+    '12:34:56:78:90:12' : 'User1',
+    '12:34:56:78:90:12' : 'User2',
+    '12:34:56:78:90:12' : 'User3',
+}
